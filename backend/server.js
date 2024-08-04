f2a28c20cdcac74449966e3bee2bd4ec647579ac
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-08-04 23:47:20 +0530

    Add REST API endpoints for fetching movies from API
    - Created Express router for movie-related endpoints.
    - Added endpoints for trending movies, movie trailers, movie details, similar movies, and movies by category.
    - Imported necessary controller functions to handle each endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 import { ENV_VARS } from "./config/enVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -11,6 +12,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); //wil allow us to parse  req.body
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
